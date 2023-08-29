365cebeef192449d17f996c8ccb4f2fe51b8dbd4
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2023-08-29 15:51:59 +0530

    WeightedAdjacencyList Updated

---
+++ Graphs/WeightedAdjacencyList.java
@@ -0,0 +1,47 @@
+package Graphs;
+
+import java.util.*;
+
+public class WeightedAdjacencyList {
+    static class Edge{
+        int src, dst, wt;
+
+        public Edge(int src, int dst, int wt) {
+            this.src = src;
+            this.dst = dst;
+            this.wt = wt;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0; i<graph.length; i++){
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0,2,10));
+
+        graph[1].add(new Edge(1,2,5));
+        graph[1].add(new Edge(1,3,7));
+
+        graph[2].add(new Edge(2,0,10));
+        graph[2].add(new Edge(2,1,5));
+        graph[2].add(new Edge(2,3,1));
+
+        graph[3].add(new Edge(3,1,7));
+        graph[3].add(new Edge(3,2,1));
+    }
+
+    public static void main(String[] args) {
+        int V = 4; //vertex in graph
+
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+
+        //print neighbors of 2
+        for(int i = 0; i<graph[2].size(); i++) {
+            Edge edge = graph[2].get(i);
+            System.out.println(edge.src + " -> " +edge.dst + " " + edge.wt);
+        }
+    
+    }
+}
